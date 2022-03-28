47e256c856a58caab4aa752d400f42c14716c187
gabmacinas <macinasgabriel@gmail.com>
2022-03-28 22:45:56 +0800

    fix: update App.js enable terms and conditions page

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ const Prediction = React.lazy(() => import('./views/game/Prediction'))
 
 const BindAccount = React.lazy(() => import('./views/utils/BindAccount'))
 // const Collections = React.lazy(() => import('./views/utils/Collections'))
-// const TermsAndConditions = React.lazy(() => import('./views/TermsAndConditions'))
+const TermsAndConditions = React.lazy(() => import('./views/TermsAndConditions'))
 
 function App () {
   // const { authenticate, user, isAuthenticated, isAuthenticating, logout } = useMoralis()
@@ -204,7 +204,7 @@ function App () {
 
           {/* <Route path='/collections' element={<Collections isAuthenticated={isAuthenticated} />} /> */}
           <Route path='/link' element={<BindAccount isAuthenticated={isAuthenticated} />} />
-          {/* <Route path='/terms' element={<TermsAndConditions />} /> */}
+          <Route path='/terms' element={<TermsAndConditions />} />
           <Route path="*" element={<Home />} />
         </Routes>
       </React.Suspense>
