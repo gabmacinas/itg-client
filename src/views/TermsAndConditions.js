431754fe5e5e123eebfa7907c12e929c0d35dc53
gabmacinas <macinasgabriel@gmail.com>
2022-03-29 12:51:46 +0800

    fix: add dompurify to implement static pages

--- src/views/TermsAndConditions.js
+++ src/views/TermsAndConditions.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import DOMPurify from 'dompurify'
+import testHtml from './html/test.html'
+const mySafeHtml = DOMPurify.sanitize(testHtml)
 
 function TermsAndConditions () {
   return (
-    <div>TermsAndConditions</div>
+    // <div>TermsAndConditions</div>
+    <div dangerouslySetInnerHTML={{ __html: mySafeHtml }}></div>
   )
 }
 
